9c328c5db68aa7546c8869efd8869000a15e671d
rookieeeee99 <zainabbubere@gmail.com>
2021-04-10 23:57:12 +0530

    Added HookCounter file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Components/Counter'
+import HookCounter from './Components/HookCounter';
 
 function App() {
   return (
     <div>
       <h1>Hooks App</h1>
       <Counter></Counter>
+      <HookCounter></HookCounter>
     </div>
   );
 }

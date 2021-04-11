a7baecec92b568d33900ca39e896a4053fdd3fa1
rookieeeee99 <zainabbubere@gmail.com>
2021-04-11 12:48:14 +0530

    Added new files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Counter from './Components/Counter'
+import EffectCounter1 from './Components/EffectCounter1';
 import HookCounter from './Components/HookCounter';
+import HookCounter3 from './Components/HookCounter3';
+import HookCounter4 from './Components/HookCounter4';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <h1>Hooks App</h1>
       <Counter></Counter>
       <HookCounter></HookCounter>
+      <HookCounter3></HookCounter3>
+      <HookCounter4></HookCounter4>
+      <EffectCounter1></EffectCounter1>
     </div>
   );
 }
